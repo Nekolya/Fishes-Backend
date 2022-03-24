ee99a8614763c55de12f6ea1b5e440ff24f11543
Olya <olya75825@gmail.com>
2022-03-24 17:19:05 +0300

    AllowAnyMethod and AllowAnyHeader

--- Program.cs
+++ Program.cs
@@ -106,7 +106,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-app.UseCors(builder => builder.AllowAnyOrigin());
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.Run();
 
